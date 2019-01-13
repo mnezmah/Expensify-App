968553db6058046a744c9b26d17e1239b93cc31e
mnezmah <mario.nezmah@gmail.com>
2019-01-13 20:46:53 +0100

    added babael-proposal-object-rest-spread plugin

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -10,16 +10,22 @@ const addExpense = (
     createdAt = 0 
   } = {}
   ) =>  ({
-  type: 'ADD_EXPENSE',
-  expense:  {
-    id: uuid(),
-    description, 
-    note, 
-    amount, 
-    createdAt
-  }
-});
+    type: 'ADD_EXPENSE',
+    expense:  {
+      id: uuid(),
+      description, 
+      note, 
+      amount, 
+      createdAt
+    }
+  });
 // REMOVE_EXPENSE
+const removeExpense = ({ id } = {}) => (
+  {
+    type: 'REMOVE_EXPENSE',
+    id
+  }
+);
 // EDIT_EXPENSE
 // SET_TEXT_FILTER
 // SORT_BY_DATE
@@ -27,13 +33,19 @@ const addExpense = (
 // SET_START_DATE
 // SET_END_DATE
 
-
 //Expenses reducer
 
 const expenseReducerDefaultState = [];
 
 const expensesReducer = (state = expenseReducerDefaultState, action) =>  {
   switch (action.type)  {
+    case 'ADD_EXPENSE':
+      return [
+        ...state, 
+        action.expense
+      ];
+    case 'REMOVE_EXPENSE':
+      return state.filter (({ id }) => id !== action.id);
     default: 
       return state;
   }
@@ -67,7 +79,11 @@ store.subscribe(() => {
   console.log(store.getState());
 });
 
-store.dispatch(addExpense({ description: 'Rent', amount:100 }));
+const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount:100 }));
+const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount:300 }));
+
+store.dispatch(removeExpense({ id: expenseOne.expense.id }));
+
 
 const demoState =  {
   expenses: [{
@@ -85,3 +101,9 @@ const demoState =  {
   }
 };
 
+const user =  {
+  name: 'Jen',
+  age: 24
+};
+
+console.log('');
