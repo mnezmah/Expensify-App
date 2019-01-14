21c4470dbde6fe60d6c8fd3a6944a5dd5cf83d1a
mnezmah <mario.nezmah@gmail.com>
2019-01-14 14:50:10 +0100

    organized redux into smaller files

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,37 +1,6 @@
 import { createStore, combineReducers } from 'redux';
 import uuid from 'uuid';
 
-// ADD_EXPENSE
-const addExpense = (
-  { 
-    description = '', 
-    note = '', 
-    amount = 0, 
-    createdAt = 0 
-  } = {}
-  ) =>  ({
-    type: 'ADD_EXPENSE',
-    expense:  {
-      id: uuid(),
-      description, 
-      note, 
-      amount, 
-      createdAt
-    }
-  });
-// REMOVE_EXPENSE
-const removeExpense = ({ id } = {}) => (
-  {
-    type: 'REMOVE_EXPENSE',
-    id
-  }
-);
-// EDIT_EXPENSE
-const editExpense = (id, updates) => ({
-  type: 'EDIT_EXPENSE',
-  id,
-  updates
-});
 
 // SET_TEXT_FILTER
 const setTextFilter = (text = '') => ({
