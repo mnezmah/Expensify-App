257d6948fa902bb459a2fb2b3fddb0f30f39db1a
mnezmah <mario.nezmah@gmail.com>
2019-01-15 10:48:13 +0100

    add expense form added

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,21 +1,16 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { removexpense, removeExpense } from '../actions/expenses';
 
-const ExpenseListItem = ({ description, amount, createdAt}) => (
+const ExpenseListItem = ({ dispatch, id, description, amount, createdAt}) => (
   <div>
       <h3>{description}</h3>
       <p>{amount} - {createdAt}</p>
+      <button onClick={() => {
+        dispatch(removeExpense({ id }));
+      }}  >Remove</button>
   </div>
 );
+  
 
-export default ExpenseListItem;
-
-//export a stateless functional component
-//that component will render: 
-//  - description
-//  - amount
-//  -  createdAt value
-// thopse three above are props passed in
-// use map() - peek in indecision code
-// Water bill and Gass bill need to fbe shown on the screen instead of 2 :)
-// you have to render many instances of expenseList item here
-// fill out the expenseList Item component file
+export default connect()(ExpenseListItem);
--- src/components/ExpenseListfilters.js
+++ src/components/ExpenseListfilters.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { setTextFilter, sortByDate, sortByAmount } from '../actions/filters'
 
-const ExpenseListFilters = () => (
+const ExpenseListFilters = (props) => (
   <div>
-    <input type="text"/>
+    <input 
+      type="text" 
+      value={props.filters.text} 
+      onChange={(e) => {
+        props.dispatch(setTextFilter(e.target.value));
+      }} 
+    />
+    <select 
+      value={props.filters.sortBy}
+      onChange={(e) => {
+        if(e.target.value === 'date') {
+          props.dispatch(sortByDate())
+        } else if (e.target.value === 'amount') {
+         props.dispatch(sortByAmount())
+        }
+      }} 
+    > 
+      <option value="date">Date</option>
+      <option value="amount">amount</option>
+    </select>
   </div>
 );
 
-export default ExpenseListFilters;
+const mapStateToProps = (state) => {
+  return {
+    filters: state.filters
+  };
+};     
+
+export default connect(mapStateToProps)(ExpenseListFilters);
