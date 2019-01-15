af0cdbef768ca39e4696025c4708c97d68c84420
mnezmah <mario.nezmah@gmail.com>
2019-01-15 08:54:53 +0100

    created  ExpenseListFilters.js

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ store.dispatch(addExpense({ description: 'Gas bill' }));
 store.dispatch(setTextFilter('water'));
 
 setTimeout(()=> {
-  store.dispatch(setTextFilter('rent'));
+  store.dispatch(setTextFilter('bill'));
 }, 3000);
 
 const state = store.getState();
--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ExpenseList from './ExpenseList';
+import ExpenseListFilters from './ExpenseListfilters';
+ 
 
 const ExpenseDashboardPage = () => (
   <div>
+    <ExpenseListFilters />
     <ExpenseList />
   </div>
 );
