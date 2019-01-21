b828328e7d43fb0ed6b73b2abf86c2356c48e782
mnezmah <mario.nezmah@gmail.com>
2019-01-21 13:23:57 +0100

    sorting and editing configured

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -10,14 +10,19 @@ console.log(now.format('MMM Do, YYYY'));
 
 
 export default class ExpenseForm extends React.Component {
-  state = {
-    description: '',
-    note: '',
-    amount: '',
-    createdAt: moment(),
-    calendarFocused: false,
-    error: ''
-  };
+  constructor(props ) {
+    super(props);
+
+    this.state = {
+      description: props.expense ? props.expense.description : '',
+      note:  props.expense ? props.expense.note : '',
+      amount: props.expense ? (props.expense.amount / 100).toString() : '',
+      createdAt: props.espense ? moment(props.expense.createdAt) :  moment(),
+      calendarFocused: false,
+      error: ''
+    }
+  }
+  
 
   onDescriptionChange = (e) =>  {
     const description = e.target.value;
@@ -53,6 +58,12 @@ export default class ExpenseForm extends React.Component {
       return <p>{this.state.error}</p>;
     } else  {
       this.setState(() => ({error: ''}));
+      this.props.onSubmit({
+        description: this.state.description,
+        amount: parseFloat(this.state.amount, 10) * 100,
+        createdAt: this.state.createdAt.valueOf(),
+        note: this.state.note
+      });
     }
   };
 
