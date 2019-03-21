3fcbacec702dd8b01a170454bf7927a55c891836
mnezmah <mario.nezmah@gmail.com>
2019-03-21 11:09:01 +0100

    Add starEditExpense

--- src/tests/components/ExpenseList.test.js
+++ src/tests/components/ExpenseList.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { ExpenseList } from '../../components/ExpenseList';
 import expenses from '../fixtures/expenses';
+import Adapter from 'enzyme-adapter-react-15';
 
 test('should render ExpenseList with expenses', () => {
   const wrapper = shallow(<ExpenseList expenses={expenses} />);
